7c250440615990d780eaba4cfa9775b66c5a0b9e
defaye <jonathanfeist@gmail.com>
2018-03-22 22:27:15 +0000

    Remove commented block from admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,23 +16,6 @@
 <body>
     @stack('body')
     <div id="app">
-        {{-- <nav class="navbar navbar-light navbar-static-top navbar-toggleable-lg">
-            @auth
-                <ul class="dropdown-menu">
-                    <li>
-                        <a href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                     document.getElementById('logout-form').submit();">
-                            Logout
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </li>
-                </ul>
-            @endauth
-        </nav> --}}
         <div id="content" class="layout-container">
             <main class="layout-content mt-3">
                 @yield('content')
